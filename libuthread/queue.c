92c1228d110eb3574592c600216b375c701f42b3
Christopher Tran <ctran209@outlook.com>
2024-02-07 08:27:48 -0800

    feat: added linked list to queue struct, finished create, destroy, and length functions

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,21 +1,48 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "queue.h"
 
+struct Node {
+	void* data;
+	struct Node* next;
+    struct Node* prev; 
+};
+
 struct queue {
-	
+	int length;
+    struct Node* head; 
+    struct Node* tail;  
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	struct queue* queue = (struct queue*)malloc(sizeof(struct queue*));
+
+	// Return NULL in case of failure when allocating the new queue.
+	if(queue == NULL){
+		return NULL;
+	}
+
+	// empty queue, set initial head and tail to NULL, and length to 0
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->length = 0;
+
+	// Return pointer to new empty queue
+	return queue; 
 }
 
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+	if(queue->length == 0){
+    	free(queue);
+		return 0;
+	}
+
+	// handle @queue is NULL or @queue is not empty case
+	else
+		return -1;
 }
 
 int queue_enqueue(queue_t queue, void *data)
@@ -40,6 +67,9 @@ int queue_iterate(queue_t queue, queue_func_t func)
 
 int queue_length(queue_t queue)
 {
-	/* TODO Phase 1 */
+	if(queue != NULL)
+		return queue->length;
+	else
+		return -1;
 }
 
