eca02976392fe7dd68a43b62da073e261cad30f1
phamkelly9 <67294673+phamkelly9@users.noreply.github.com>
2024-02-16 18:37:24 -0800

    W

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -16,6 +16,11 @@
 
 
 struct sigaction sa;
+struct sigaction curr_sa;
+
+sigset_t ss;
+
+struct itimerval old_timer;
 struct itimerval timer;
 bool isPreemptEnabled = false;
 
@@ -29,28 +34,20 @@ void handler(int signum) {
 void preempt_disable(void)
 {
     if(isPreemptEnabled == true){
-        sigset_t set;
-
-        // Initialize an empty signal set to SIGVTALRM
-        sigemptyset(&set);
-        sigaddset(&set, SIGVTALRM);
 
         // unblock thread 
-        sigprocmask(SIG_BLOCK, &set, NULL);
+        sigprocmask(SIG_BLOCK, &ss, NULL);
     }
 }
 
 void preempt_enable(void)
 {
     if(isPreemptEnabled == true){
-        sigset_t set;
 
         // Initialize an empty signal set to SIGVTALRM
-        sigemptyset(&set);
-        sigaddset(&set, SIGVTALRM);
 
         // unblock thread 
-        sigprocmask(SIG_UNBLOCK, &set, NULL);
+        sigprocmask(SIG_UNBLOCK, &ss, NULL);
     }
 }
 
@@ -59,8 +56,6 @@ void preempt_start(bool preempt)
 	if(preempt){
 		preempt_enable();
         isPreemptEnabled = preempt;
-        struct sigaction curr_sa;
-        struct itimerval curr_timer;
 
         // Set up the signal handler for SIGVTALRM
         curr_sa.sa_handler = handler;
@@ -68,21 +63,24 @@ void preempt_start(bool preempt)
         curr_sa.sa_flags = 0;
         sigaction(SIGVTALRM, &curr_sa, &sa);
 
+        sigemptyset(&ss);
+        sigaddset(&ss, SIGVTALRM);
+
         // Configure the timer to fire at 100 Hz
         timer.it_value.tv_sec = 0;
         timer.it_value.tv_usec = HZ * 100; 
         timer.it_interval.tv_sec = 0;
         timer.it_interval.tv_usec = HZ * 100; 
 
-        setitimer(ITIMER_VIRTUAL, &curr_timer, &timer);
+        setitimer(ITIMER_VIRTUAL, &timer, &old_timer);
     }
 }
 
 void preempt_stop(void)
 {
     if(isPreemptEnabled){
-        sigaction(SIGPROF, &sa, NULL);
-        setitimer(ITIMER_VIRTUAL, &timer, NULL);
+        sigaction(SIGVTALRM, &sa, NULL);
+        setitimer(ITIMER_VIRTUAL, &old_timer, NULL);
         isPreemptEnabled = false;
     }
 }
